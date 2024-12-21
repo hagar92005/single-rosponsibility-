70e8590bddc540719fb81a5771191a3e0107d7de
hagar92005 <hager92005@gmail.com>
2024-12-21 23:27:56 +0200

    Add files via upload

---
+++ isp.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 21 23:13:19 2024
+
+@author: PCZHRAA
+"""
+
+#isp
+
+from abc import ABC, abstractmethod 
+ 
+class Printer(ABC): 
+    @abstractmethod 
+    def print(self, document): 
+        pass 
+ 
+class Fax(ABC): 
+    @abstractmethod 
+    def fax(self, document): 
+        pass 
+ 
+class Scanner(ABC): 
+    @abstractmethod 
+    def scan(self, document): 
+        pass 
+ 
+class OldPrinter(Printer): 
+    def print(self, document): 
+        print(f"Printing {document} in black and white...") 
+ 
+class NewPrinter(Printer, Fax, Scanner): 
+    def print(self, document): 
+        print(f"Printing {document} in color...") 
+ 
+    def fax(self, document): 
+        print(f"Faxing {document}...") 
+ 
+    def scan(self, document): 
+        print(f"Scanning {document}...") 
