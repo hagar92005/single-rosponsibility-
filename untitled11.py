869a73f6c87321b1b4b6ef5e871e6d93be961f3a
hagar92005 <hager92005@gmail.com>
2024-12-21 23:07:43 +0200

    Add files via upload

---
+++ untitled11.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 12:09:17 2024
+
+@author: PCZHRAA
+"""
+#ocp
+from abc import ABC, abstractmethod 
+from math import pi 
+class Shape(ABC): 
+    def __init__(self, shape_type): 
+        self.shape_type = shape_type 
+ 
+    @abstractmethod 
+    def calculate_area(self): 
+        pass 
+ 
+class Circle(Shape): 
+    def __init__(self, radius): 
+        super().__init__("circle") 
+        self.radius = radius 
+ 
+    def calculate_area(self): 
+        return pi * self.radius**2 
+ 
+class Rectangle(Shape): 
+    def __init__(self, width, height): 
+        super().__init__("rectangle") 
+        self.width = width 
+        self.height = height 
+ 
+    def calculate_area(self): 
+        return self.width * self.height
