9c9b90447a50a2b0870d0a9d100647482ae8bb9a
hagar92005 <hager92005@gmail.com>
2024-12-23 08:33:00 +0200

    Update Polymorphism.py

--- Polymorphism.py
+++ Polymorphism.py
@@ -28,9 +28,8 @@ class Plane:
     def move(self):
         print("Fly!")
 
-car1 = Car("Ford", "Mustang")       #Create a Car class
-boat1 = Boat("Ibiza", "Touring 20") #Create a Boat class
-plane1 = Plane("Boeing", "747")     #Create a Plane class
+car1 = Car("Ford", "Mustang")       
+boat1 = Boat("Ibiza", "Touring 20") 
+plane1 = Plane("Boeing", "747")     
 
-for x in (car1, boat1, plane1):
-   x.move()
+
